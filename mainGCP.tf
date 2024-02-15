3ae88ec7e8f1b98e7a77ea4f292e6b54ccfaddf9
Vishnu Paduchuri <paduchuri.v@northeastern.edu>
2024-02-15 14:59:01 -0500

    Formatted code using terraform fmt and added github workflow

--- mainGCP.tf
+++ mainGCP.tf
@@ -1,32 +1,32 @@
 provider "google" {
   credentials = file(var.gcp_credentials)
-  project = var.gcp_project_id
-  region = var.gcp_project_region
+  project     = var.gcp_project_id
+  region      = var.gcp_project_region
 }
 
 resource "google_compute_network" "vpc" {
-  name = var.vpc_name
-  auto_create_subnetworks = var.vpc_create_subnets_automatically
-  routing_mode = var.routing_mode_for_vpc
+  name                            = var.vpc_name
+  auto_create_subnetworks         = var.vpc_create_subnets_automatically
+  routing_mode                    = var.routing_mode_for_vpc
   delete_default_routes_on_create = var.vpc_delete_default_routes_on_create
 }
 
 resource "google_compute_subnetwork" "webapp" {
-    name = var.webapp_subnet_name
-    ip_cidr_range = var.webapp_subnet_ip_cidr_range
-    network = google_compute_network.vpc.self_link
+  name          = var.webapp_subnet_name
+  ip_cidr_range = var.webapp_subnet_ip_cidr_range
+  network       = google_compute_network.vpc.self_link
 }
 
 resource "google_compute_subnetwork" "db" {
-  name = var.db_subnet_name
+  name          = var.db_subnet_name
   ip_cidr_range = var.db_subnet_ip_cidr_range
-  network = google_compute_network.vpc.self_link
+  network       = google_compute_network.vpc.self_link
 }
 
 resource "google_compute_route" "webapp_subnet_route" {
-  network = google_compute_network.vpc.self_link
-  name = var.webapp_subnet_route_name
-  dest_range = var.webapp_subnet_route_dest_range
+  network          = google_compute_network.vpc.self_link
+  name             = var.webapp_subnet_route_name
+  dest_range       = var.webapp_subnet_route_dest_range
   next_hop_gateway = var.webapp_subnet_route_next_hop_gateway
-  priority = var.webapp_subnet_route_priority
+  priority         = var.webapp_subnet_route_priority
 }
--- variables.tf
+++ variables.tf
@@ -3,76 +3,76 @@
 # https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-variables
 
 variable "gcp_credentials" {
-    description = "GCP credentials for the project"
-    type=string
+  description = "GCP credentials for the project"
+  type        = string
 }
 
 variable "gcp_project_id" {
-    description="The project id where the resources will be created"
-    type=string
+  description = "The project id where the resources will be created"
+  type        = string
 }
 
 variable "gcp_project_region" {
-    description="GCP project region"
-    type=string
+  description = "GCP project region"
+  type        = string
 }
 
 variable "vpc_name" {
-    description="VCP Name"
-    type=string
+  description = "VCP Name"
+  type        = string
 }
 
 variable "vpc_create_subnets_automatically" {
-    description="This indicates if subnets should be created automatically or not"
-    type=bool
+  description = "This indicates if subnets should be created automatically or not"
+  type        = bool
 }
 
 variable "routing_mode_for_vpc" {
-    description="Routing mode for the VPC"
-    type=string
+  description = "Routing mode for the VPC"
+  type        = string
 }
 
 variable "vpc_delete_default_routes_on_create" {
-    description="Delete default routes on create"
-    type=bool
+  description = "Delete default routes on create"
+  type        = bool
 }
 
-variable "webapp_subnet_name"{
-    description="The name of the webapp subnet"
-    type=string
+variable "webapp_subnet_name" {
+  description = "The name of the webapp subnet"
+  type        = string
 }
 
 variable "webapp_subnet_ip_cidr_range" {
-    description="The IP CIDR range of the webapp subnet"
-    type=string
+  description = "The IP CIDR range of the webapp subnet"
+  type        = string
 }
 
-variable "db_subnet_name"{
-    description="The name of the DB subnet"
-    type=string
+variable "db_subnet_name" {
+  description = "The name of the DB subnet"
+  type        = string
 }
 
 variable "db_subnet_ip_cidr_range" {
-    description="The IP CIDR range of the DB subnet"
-    type=string
+  description = "The IP CIDR range of the DB subnet"
+  type        = string
 }
 
 variable "webapp_subnet_route_name" {
-    description="The name given to the webapp subnet route"
-    type=string
+  description = "The name given to the webapp subnet route"
+  type        = string
 }
 
 variable "webapp_subnet_route_dest_range" {
-    description="The destination range for the webapp subnet route"
-    type=string
+  description = "The destination range for the webapp subnet route"
+  type        = string
 }
 
 variable "webapp_subnet_route_next_hop_gateway" {
-    description="The next hop gateway for the webapp subnet route"
-    type=string
+  description = "The next hop gateway for the webapp subnet route"
+  type        = string
 }
 
 variable "webapp_subnet_route_priority" {
-    description="Delete default routes on create"
-    type=number
+  description = "Delete default routes on create"
+  type        = number
 }
